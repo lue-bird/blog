9ca024ba6a7743d61119596faca484a1e393dad6
lue-bird <lue.the.bird@gmail.com>
2023-10-19 20:46:11 +0200

    font sizes adjust

--- src/Main.elm
+++ src/Main.elm
@@ -226,13 +226,11 @@ ui _ =
         ]
         [ Articles.all
             |> articleContentUi
-        , Html.node "style" [] [ Html.text """.elmsh {color: #ffffff;background: #000000;}.elmsh-hl {background: #343434;}.elmsh-add {background: #003800;}.elmsh-del {background: #380000;}.elmsh-comm {color: #75715e;}.elmsh1 {color: #ae81ff;}.elmsh2 {color: #e6db74;}.elmsh3 {color: #f92672;}.elmsh4 {color: #66d9ef;}.elmsh5 {color: #a6e22e;}.elmsh6 {color: #ae81ff;}.elmsh7 {color: #fd971f;}.elmsh-elm-ts, .elmsh-js-dk, .elmsh-css-p {font-style: italic;color: #66d9ef;}.elmsh-js-ce {font-style: italic;color: #a6e22e;}.elmsh-css-ar-i {font-weight: bold;color: #f92672;}""" ]
-            |> Element.WithContext.html
         ]
         |> Element.WithContext.layout {}
             [ Element.WithContext.Background.color (Element.WithContext.rgb 0 0 0)
             , Element.WithContext.Font.color (Element.WithContext.rgb 1 1 1)
-            , Element.WithContext.Font.size 22
+            , Element.WithContext.Font.size 20
             ]
 
 
@@ -297,6 +295,7 @@ articleContentUi =
                         , Html.Attributes.style "word-break" "normal"
                         , Html.Attributes.style "overflow-wrap" "normal"
                         , Html.Attributes.style "hyphens" "none"
+                        , Html.Attributes.style "font-size" "0.7em"
                         ]
                         [ ElmCodeUi.with elmCode.syntaxKindMap elmCode.raw ]
                     ]
