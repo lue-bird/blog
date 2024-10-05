7a9125d3a1afbffdf526006db86291a7e47f81d1
lue-bird <lue.the.bird@gmail.com>
2024-10-05 22:57:24 +0200

    remove unused Reaction members

--- src/Reaction.elm
+++ src/Reaction.elm
@@ -2,12 +2,9 @@ module Reaction exposing
     ( Reaction
     , to
     , effectsAdd
-    , map, effectMap
-    , effects, state
-    , toTuple2, toTuple3
+    , toTuple3
     , EffectInterpretation
     , commands, audioCommands
-    , effectInterpretationMap
     )
 
 {-|
@@ -23,24 +20,17 @@ module Reaction exposing
 ## alter
 
 @docs effectsAdd
-@docs map, effectMap
-
-
-## scan
-
-@docs effects, state
 
 
 ## transform
 
-@docs toTuple2, toTuple3
+@docs toTuple3
 
 
 ### effect interpretation
 
 @docs EffectInterpretation
 @docs commands, audioCommands
-@docs effectInterpretationMap
 
 -}
 
@@ -76,31 +66,6 @@ to stateAltered =
     }
 
 
-map :
-    (state -> stateMapped)
-    ->
-        (Reaction state effect
-         -> Reaction stateMapped effect
-        )
-map stateMap =
-    \step ->
-        to (step |> state |> stateMap)
-            |> effectsAdd (step |> effects)
-
-
-effectMap :
-    (effect -> effectMapped)
-    ->
-        (Reaction state effect
-         -> Reaction state effectMapped
-        )
-effectMap effectChange =
-    \reaction ->
-        { state = reaction |> state
-        , effects = reaction |> effects |> List.map effectChange
-        }
-
-
 effectsAdd :
     List effect
     -> (Reaction state effect -> Reaction state effect)
@@ -113,11 +78,7 @@ effectsAdd effectsAdditional =
 
 
 {-| Last step before giving your init/update functions to the audio app:
-3-tuple from
-
-  - [`state`](#state)
-  - [`effects`](#effects) as first `Cmd`s, then `AudioCmd`s
-
+3-tuple from state, effects as first `Cmd`s, then `AudioCmd`s
 -}
 toTuple3 :
     (effect -> EffectInterpretation event)
@@ -142,32 +103,6 @@ toTuple3 interpretEffect =
         )
 
 
-{-| Last step before giving your init/update functions to the app:
-2-tuple from
-
-  - [`state`](#state)
-  - [`effects`](#effects) as `Cmd`s batched
-
--}
-toTuple2 :
-    (effect -> List (Cmd event))
-    ->
-        (Reaction state effect
-         ->
-            ( state
-            , Cmd event
-            )
-        )
-toTuple2 interpretEffect =
-    \step ->
-        ( step |> state
-        , step
-            |> effects
-            |> List.concatMap interpretEffect
-            |> Cmd.batch
-        )
-
-
 {-| Interpretation of an effect as `Cmd`s and `AudioCmd`s.
 -}
 type alias EffectInterpretation event =
@@ -184,13 +119,3 @@ commands commandList =
 audioCommands : List (Audio.AudioCmd event) -> EffectInterpretation event
 audioCommands audioCommandList =
     { commands = [], audioCommands = audioCommandList }
-
-
-effectInterpretationMap :
-    (event -> eventMapped)
-    -> (EffectInterpretation event -> EffectInterpretation eventMapped)
-effectInterpretationMap eventChange =
-    \effectInterpretation ->
-        { commands = effectInterpretation.commands |> List.map (Cmd.map eventChange)
-        , audioCommands = effectInterpretation.audioCommands |> List.map (Audio.cmdMap eventChange)
-        }
