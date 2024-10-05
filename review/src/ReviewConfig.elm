740d891777e14fbb9db3048f92a5b258009912a5
lue-bird <lue.the.bird@gmail.com>
2024-10-05 22:48:08 +0200

    update review config

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -9,149 +9,139 @@ To add packages that contain rules, add them to this review project using
 
 when inside the directory containing this file.
 
-Rules on radar
-
-  - [`jfmengels/elm-review-cognitive-complexity`](https://dark.elm.dmy.fr/packages/jfmengels/elm-review-cognitive-complexity/latest/CognitiveComplexity)
-      - I currently fear that there will be
-        exceptions where we can't
-        abstract/simplify functions further
-
-
-## split on
-
-
-## rejected
-
-    Pipeline.parentheticalApplicationPipelines
-        |> Pipeline.forbid
-        |> Pipeline.that
-            (PipelinePredicate.haveAnyNonInputStepThatIs
-                PipelinePredicate.aSemanticallyInfixFunction
-            )
-        |> Pipeline.andTryToFixThemBy PipelineFix.convertingToRightPizza
-        |> Pipeline.andCallThem "parenthetical application of a semantically-infix function"
-
-because `aSemanticallyInfixFunction` covers `atLeast`/`atMost`/... which can be used here: `MorphRow.atLeast n3 AToZ.char`
-
-    Pipeline.rightCompositionPipelines
-        |> Pipeline.forbid
-        |> Pipeline.andReportCustomError
-            ">> pipeline"
-            [ "Forbidding `g >> f` for reasons of simplicity, consistency:"
-            , [ "Establish a subject: `List.map (\\user -> user |> User.badgeAdd ... |> User.levelIncrease)`"
-              , " for easier readability and scalability (maybe even creating a separate function)"
-              , " when chaining multiple operations"
-              ]
-                |> String.concat
-            ]
-
-because they can improve food scoping
-
-  - [`truqu/elm-review-nobooleancase`](https://dark.elm.dmy.fr/packages/truqu/elm-review-nobooleancase/latest/)
-    preferably, I'd completely remove `Bool`s and with it `if ... then ... else ...`
-      - completely covered by elm-review-simplify
-
 -}
 
 import Docs.NoMissing
 import Docs.ReviewAtDocs
 import Docs.ReviewLinksAndSections
+import Docs.UpToDateReadmeLinks
+import EqualsCaseable
+import MultipleAppendToConcat
 import NoAlways
+import NoCatchAllForSpecificRemainingPatterns
 import NoDebug.Log
 import NoDebug.TodoOrToString
+import NoDeprecated
+import NoDuplicatePorts
 import NoExposingEverything
 import NoForbiddenWords
-import NoFunctionOutsideOfModules
-import NoImportingEverything
 import NoMissingTypeAnnotation
+import NoMissingTypeAnnotationInLetIn
 import NoMissingTypeExpose
+import NoPrematureLetComputation
 import NoPrimitiveTypeAlias
 import NoRecordAliasConstructor
+import NoRecursiveUpdate
+import NoSimpleLetBody
 import NoSinglePatternCase
-import NoUnsortedLetDeclarations
+import NoUnnecessaryTrailingUnderscore
+import NoUnoptimizedRecursion
+import NoUnsafeDivision
+import NoUnsafePorts
+import NoUnsortedCases
 import NoUnsortedTopLevelDeclarations
 import NoUnused.CustomTypeConstructorArgs
 import NoUnused.CustomTypeConstructors
 import NoUnused.Dependencies
 import NoUnused.Exports
-import NoUnused.Modules
 import NoUnused.Parameters
 import NoUnused.Patterns
 import NoUnused.Variables
+import NoUnusedPorts
 import OnlyAllSingleUseTypeVarsEndWith_
-import Review.Rule as Rule exposing (Rule)
-import ReviewPipelineStyles as Pipeline
-import ReviewPipelineStyles.Fixes as PipelineFix
-import ReviewPipelineStyles.Predicates as PipelinePredicate
-import ReviewPipelineStyles.Premade as Pipeline
+import Review.Documentation.CodeSnippet
+import Review.ImportSimple
+import Review.OpaqueType
+import Review.Pattern.As
+import Review.Pattern.Record
+import Review.PhantomType
+import Review.Rule
+import Review.VariantValueCount
+import ReviewPipelineStyles
+import ReviewPipelineStyles.Fixes
+import ReviewPipelineStyles.Predicates
 import Simplify
+import UseCamelCase
+import VariablesBetweenCaseOf.AccessInCases
 
 
-config : List Rule
+config : List Review.Rule.Rule
 config =
     [ -- ## documentation
+      -- enable on a per-project basis
+      --, Review.Documentation.CodeSnippet.check
       Docs.ReviewLinksAndSections.rule
     , Docs.ReviewAtDocs.rule
     , Docs.NoMissing.rule
         { document = Docs.NoMissing.onlyExposed
         , from = Docs.NoMissing.exposedModules
         }
+    , Docs.UpToDateReadmeLinks.rule
 
     -- ## simplify
-    , NoUnused.CustomTypeConstructors.rule []
-    , NoUnused.CustomTypeConstructorArgs.rule
     , NoUnused.Dependencies.rule
     , NoUnused.Exports.rule
-    , NoUnused.Modules.rule
     , NoUnused.Parameters.rule
     , NoUnused.Patterns.rule
     , NoUnused.Variables.rule
-    , Simplify.rule Simplify.defaults
-    , NoSinglePatternCase.rule
-        (NoSinglePatternCase.fixInArgument
-            |> NoSinglePatternCase.ifAsPatternRequired
-                (NoSinglePatternCase.fixInLetInstead
-                    |> NoSinglePatternCase.andIfNoLetExists
-                        NoSinglePatternCase.createNewLet
-                )
+    , NoUnusedPorts.rule
+    , Simplify.rule
+        (Simplify.defaults |> Simplify.expectNaN)
+    , MultipleAppendToConcat.rule MultipleAppendToConcat.PipeRightList
+
+    -- ## sort
+    , NoUnsortedTopLevelDeclarations.rule
+        (NoUnsortedTopLevelDeclarations.sortTopLevelDeclarations
+            |> NoUnsortedTopLevelDeclarations.glueHelpersAfter
         )
 
     -- ## limit
-    , [ [ [ Pipeline.rightPizzaPipelines
-                |> Pipeline.forbid
-                |> Pipeline.that
-                    (PipelinePredicate.haveAnyStepThatIs
-                        PipelinePredicate.aConfusingNonCommutativeFunction
-                    )
-                |> Pipeline.andCallThem
-                    "|> pipeline with confusing non-commutative function"
-          , Pipeline.parentheticalApplicationPipelines
-                |> Pipeline.forbid
-                |> Pipeline.that
-                    (PipelinePredicate.haveAnyStepThatIs
-                        PipelinePredicate.aConfusingNonCommutativePrefixOperator
-                    )
-                |> Pipeline.andCallThem
-                    "parenthetical application with confusing non-commutative prefix operator"
-          ]
-        ]
-            |> List.concat
-      , [ Pipeline.leftPizzaPipelines
-            |> Pipeline.forbid
-            |> Pipeline.andTryToFixThemBy
-                PipelineFix.convertingToParentheticalApplication
-            |> Pipeline.andReportCustomError
+    , NoUnused.CustomTypeConstructors.rule []
+    , NoUnused.CustomTypeConstructorArgs.rule
+    , Review.VariantValueCount.zeroOrOne
+    , [ ReviewPipelineStyles.rightPizzaPipelines
+            |> ReviewPipelineStyles.forbid
+            |> ReviewPipelineStyles.that
+                (ReviewPipelineStyles.Predicates.haveAnyStepThatIs
+                    ReviewPipelineStyles.Predicates.aConfusingNonCommutativeFunction
+                )
+            |> ReviewPipelineStyles.andCallThem
+                "|> pipeline with confusing non-commutative function"
+      , ReviewPipelineStyles.parentheticalApplicationPipelines
+            |> ReviewPipelineStyles.forbid
+            |> ReviewPipelineStyles.that
+                (ReviewPipelineStyles.Predicates.haveAnyStepThatIs
+                    ReviewPipelineStyles.Predicates.aConfusingNonCommutativePrefixOperator
+                )
+            |> ReviewPipelineStyles.andCallThem
+                "parenthetical application with confusing non-commutative prefix operator"
+      , ReviewPipelineStyles.leftPizzaPipelines
+            |> ReviewPipelineStyles.forbid
+            |> ReviewPipelineStyles.andTryToFixThemBy
+                ReviewPipelineStyles.Fixes.convertingToParentheticalApplication
+            |> ReviewPipelineStyles.andReportCustomError
                 "<| pipeline"
                 [ "Forbidding `f <| a s` for reasons of simplicity, consistency:"
                 , "  - Pipe data before the function: `food |> op ...`"
                 , "  - Feed arguments after the function: `... |> opWith (a ...) (b ...)`"
                 , "Use the application style `f (a s)` instead"
                 ]
-        , Pipeline.leftCompositionPipelines
-            |> Pipeline.forbid
-            |> Pipeline.andReportCustomError
+      , ReviewPipelineStyles.rightCompositionPipelines
+            |> ReviewPipelineStyles.forbid
+            |> ReviewPipelineStyles.andReportCustomError
+                ">> pipeline"
+                [ "Avoid `g >> f` for easier to understand, more consistent code:"
+                , [ "Establish a subject: `List.map (\\user -> user |> User.badgeAdd ... |> User.levelIncrease)`"
+                  , " for easier readability and scalability (maybe even creating a separate function)"
+                  , " when chaining multiple operations"
+                  ]
+                    |> String.concat
+                ]
+      , ReviewPipelineStyles.leftCompositionPipelines
+            |> ReviewPipelineStyles.forbid
+            |> ReviewPipelineStyles.andReportCustomError
                 "<< pipeline"
-                [ "Forbidding `g << f` for reasons of simplicity, readability, consistency:"
+                [ "Avoid `g << f` for easier to understand, more consistent code:"
                 , "  - Keep the order data comes from before and gets piped through functions after: `... |> opF |> opG`"
                 , [ "Establish a subject: `List.map (\\user -> user |> User.badgeAdd ... |> User.levelIncrease)`"
                   , " for easier readability and scalability (maybe even creating a separate function)"
@@ -159,66 +149,115 @@ config =
                   ]
                     |> String.concat
                 ]
-        ]
+      , ReviewPipelineStyles.parentheticalApplicationPipelines
+            |> ReviewPipelineStyles.forbid
+            |> ReviewPipelineStyles.that
+                (ReviewPipelineStyles.Predicates.haveAnyNonInputStepThatIs
+                    ReviewPipelineStyles.Predicates.aSemanticallyInfixFunction
+                )
+            |> ReviewPipelineStyles.andTryToFixThemBy ReviewPipelineStyles.Fixes.convertingToRightPizza
+            |> ReviewPipelineStyles.andCallThem "parenthetical application of a semantically-infix function"
       ]
-        |> List.concat
-        |> Pipeline.rule
+        |> ReviewPipelineStyles.rule
+    , UseCamelCase.rule
+        (UseCamelCase.default
+            |> UseCamelCase.withCamel toCamelCase
+            |> UseCamelCase.withPascal toCamelCase
+        )
     , NoPrimitiveTypeAlias.rule
+    , Review.ImportSimple.rule
     , OnlyAllSingleUseTypeVarsEndWith_.rule
     , NoRecordAliasConstructor.rule
-    , NoDebug.TodoOrToString.rule
-        |> Rule.ignoreErrorsForDirectories [ "tests/" ]
     , NoExposingEverything.rule
     , NoForbiddenWords.rule forbiddenWords
-    , NoImportingEverything.rule []
     , NoMissingTypeAnnotation.rule
+    , NoMissingTypeAnnotationInLetIn.rule
     , NoMissingTypeExpose.rule
-    , NoUnsortedTopLevelDeclarations.rule
-        (NoUnsortedTopLevelDeclarations.sortTopLevelDeclarations
-            |> NoUnsortedTopLevelDeclarations.glueHelpersAfter
-            |> NoUnsortedTopLevelDeclarations.glueDependenciesBeforeFirstDependent
-        )
-    , NoUnsortedLetDeclarations.rule
-        (NoUnsortedLetDeclarations.sortLetDeclarations
-            |> NoUnsortedLetDeclarations.glueDependenciesBeforeFirstDependent
-        )
-    , NoFunctionOutsideOfModules.rule
-        [ ( forbiddenFunctionOrValues, [] ) ]
     , NoAlways.rule
-    , -- could be included in the above
-      NoDebug.Log.rule
+    , NoDebug.Log.rule
+        |> Review.Rule.ignoreErrorsForDirectories [ "tests/" ]
+    , NoDebug.TodoOrToString.rule
+        |> Review.Rule.ignoreErrorsForDirectories [ "tests/" ]
+    , VariablesBetweenCaseOf.AccessInCases.forbid
+    , EqualsCaseable.forbid EqualsCaseable.Everywhere
+    , NoDeprecated.rule NoDeprecated.defaults
+    , NoPrematureLetComputation.rule
+    , NoDuplicatePorts.rule
+    , NoUnsafePorts.rule NoUnsafePorts.any
+    , NoRecursiveUpdate.rule
+    , NoUnoptimizedRecursion.optOutWithComment "IGNORE TCO"
+        |> NoUnoptimizedRecursion.rule
+        |> Review.Rule.ignoreErrorsForDirectories [ "tests/" ]
+    , NoSimpleLetBody.rule
+    , NoUnnecessaryTrailingUnderscore.rule
+    , NoUnsafeDivision.rule
+    , Review.Pattern.Record.forbid
+    , Review.Pattern.As.forbid
+    , Review.PhantomType.forbid
+    , Review.OpaqueType.forbid
+    , NoCatchAllForSpecificRemainingPatterns.rule
     ]
-
-
-forbiddenFunctionOrValues : List String
-forbiddenFunctionOrValues =
-    [ -- use tuple destructuring instead
-      -- for improved descriptiveness
-      "Tuple.first"
-    , "Tuple.second"
-    , -- use `mapFirst |> mapSecond` instead
-      "Tuple.mapBoth"
-    , -- use `String.indexes` instead
-      "String.indices"
-    , -- use a `case` instead
-      "String.isEmpty"
-    , "List.isEmpty"
-    , "List.tail"
-
-    -- use a `Set`, `Dict` or `List.sortWith`
-    , "List.sort"
-    , "List.sortBy"
-    ]
+        |> List.map (Review.Rule.ignoreErrorsForDirectories [ "VerifyExamples/" ])
 
 
 forbiddenWords : List String
 forbiddenWords =
-    [ [ "REPLACEME", "replaceme", "replace-me", "ReplaceMe" ]
-    , [ "ToReplace", "TOREPLACE", "to-replace" ]
-    , [ "TODO", "todo", "Todo", "to-do", "ToDo" ]
-    , [ "- []" ]
-    , [ "ToCheck", "to-check" ]
-    , [ "ToFix", "TOFIX" ]
-    , [ "FIXME", "fixme", "FixMe", "Fixme" ]
-    ]
-        |> List.concat
+    [ "REPLACEME", "TODO", "- []" ]
+
+
+toCamelCase : String -> String
+toCamelCase =
+    \name ->
+        let
+            camelFolded : { camelized : String, upperCaseNextRequired : Maybe { underscores : Int } }
+            camelFolded =
+                name
+                    |> String.foldl
+                        (\char soFar ->
+                            if char |> Char.isUpper then
+                                { upperCaseNextRequired = Nothing
+                                , camelized = soFar.camelized ++ (char |> String.fromChar)
+                                }
+
+                            else if char |> Char.isLower then
+                                { upperCaseNextRequired = Nothing
+                                , camelized =
+                                    case soFar.upperCaseNextRequired of
+                                        Just _ ->
+                                            soFar.camelized ++ (char |> Char.toUpper |> String.fromChar)
+
+                                        Nothing ->
+                                            soFar.camelized ++ (char |> String.fromChar)
+                                }
+
+                            else
+                                case char of
+                                    '_' ->
+                                        { upperCaseNextRequired =
+                                            Just
+                                                { underscores =
+                                                    case soFar.upperCaseNextRequired of
+                                                        Nothing ->
+                                                            1
+
+                                                        Just trail ->
+                                                            trail.underscores + 1
+                                                }
+                                        , camelized = soFar.camelized
+                                        }
+
+                                    nonLetterNonUnderscoreChar ->
+                                        { upperCaseNextRequired = Nothing
+                                        , camelized = soFar.camelized ++ (nonLetterNonUnderscoreChar |> String.fromChar)
+                                        }
+                        )
+                        { camelized = ""
+                        , upperCaseNextRequired = Nothing
+                        }
+        in
+        case camelFolded.upperCaseNextRequired of
+            Nothing ->
+                camelFolded.camelized
+
+            Just trail ->
+                camelFolded.camelized ++ String.repeat trail.underscores "_"
